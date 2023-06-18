b1bf0b55d64665957cd81cc78c740b6774c079eb
Abhinav Rai <111674442+AbhinavRai2004@users.noreply.github.com>
2023-06-18 09:58:14 +0530

    props and state

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,78 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+
+// event-function.
+
+// function App() {
+//     let data = "anil sidhu";
+
+//     function apple() {
+//         data = "peter";
+//         alert(data);
+//     }
+//     return ( <
+//         div className = "App" >
+//         <
+//         h1 > { data } < /h1> <
+//         button onClick = { apple } > Click Me < /button> < /
+//         div >
+//     );
+// }
+// export default App;
+
+
+
+// State in functional component in reactjs.
+
+
+// import { useState } from 'react'
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//     const [data, setData] = useState(0)
+
+//     function updateData() {
+//         setData(data + 1)
+
+//     }
+//     console.warn("______");
+//     return ( <
+//         div className = "App" >
+//         <
+//         h1 > { data } < /h1> <
+//         button onClick = { updateData } > Update Data < /button> < /
+//         div >
+//     );
+// }
+
+// export default App;
+
+
+// props in functional component in react.
+
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Student from './Student'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState("Abhinav")
+
+    function apple() {
+        alert("hello")
+    }
+    return ( <
+            div className = "App" >
+            <
+            h1 > Props in React: ) < /h1>  <
+        Student name = { name }
+    /> <
+    button onClick = {
+            () => { setName("Rai") } } > Update Name < /button> <
+        /div >
+);
 }
 
 export default App;
---
+++ blog/src/User.js
@@ -0,0 +1,10 @@
+function User() {
+    return ( <
+        div className = "App" >
+        <
+        h1 > This is Functional Component < /h1> </
+        div >
+    );
+}
+
+export default User;
